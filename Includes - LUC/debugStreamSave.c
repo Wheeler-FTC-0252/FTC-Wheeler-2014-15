1c90d18738b6adcfad12849ecccb535acfd2ad1b
Lucien Gaitskell <lucien@gaitskell.com>
2015-03-02 10:44:28 -0500

    added init for debug save

--- Includes - LUC/debugStreamSave.c
+++ Includes - LUC/debugStreamSave.c
@@ -1,3 +1,42 @@
+TFileHandle saveFileInit(){//run this at the beginning before using the functions below
+	bool fileNameFound=False;
+	string fileName;
+	string fileBeginning="debug-";
+	string fileMiddle=__DATE__;
+	fileMiddle += __TIME__;
+	fileMiddle += "-"
+	string fileEnding=0;
+	string fileExtention=".txt"
+
+	TFileHandle searchHandle;
+	TFileIOResult searchResult;
+	string searchReturnName;
+	short searchFileSize;
+	string searchName;
+
+	while (!fileNameFound){
+		fileEnding++;
+		searchName = fileBeginning;
+		searchName += fileMiddle;
+		searchName += fileEnding;
+		searchName += fileExtention;
+
+		FindFirstFile(searchHandle, searchResult, searchName,  searchReturnName, searchFileSize) ;
+		if (searchResult==0){
+			fileName=searchName;
+			fileNameFound=True;
+		}
+	}
+
+
+	TFileIOResult IoResult;
+	TFileHandle FileHandle;
+	short FileSize = 100;
+
+	OpenWrite(FileHandle, IoResult, fileName, FileSize);
+	return FileHandle
+}
+
 void writeDebugStreamSaveMain(char * saveText, bool carrageReturn){
 	TFileIOResult IoResult;
 	TFileHandle FileHandle;
